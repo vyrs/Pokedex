87e8a25abba2892832d00dff08656b2a63326e9e
Vitor Yuri <vitoryuri@pop-os.localdomain>
2020-10-04 11:15:44 -0300

    projeto pokedex

---
+++ src/Pokedex.jsx
@@ -0,0 +1,135 @@
+import React, { useEffect, useState } from "react";
+import {
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CircularProgress,
+  Toolbar,
+  AppBar,
+  TextField,
+} from "@material-ui/core";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import { toFirstCharUppercase } from "./constants";
+import SearchIcon from "@material-ui/icons/Search";
+import axios from "axios";
+
+const useStyles = makeStyles((theme) => ({
+  pokedexContainer: {
+    paddingTop: "20px",
+    paddingLeft: "50px",
+    paddingRight: "50px",
+  },
+  cardMedia: {
+    margin: "auto",
+  },
+  cardContent: {
+    textAlign: "center",
+  },
+  searchContainer: {
+    display: "flex",
+    color: "#000",
+    backgroundColor: "#fff",
+    // backgroundColor: fade(theme.palette.common.white, 0.15),
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    margin: "10px auto",
+  },
+  searchIcon: {
+    alignSelf: "flex-end",
+    marginBottom: "5px",
+  },
+  searchInput: {
+    width: "400px",
+    margin: "5px",
+  },
+}));
+
+const Pokedex = (props) => {
+  const classes = useStyles();
+  const { history } = props;
+  const [pokemonData, setPokemonData] = useState({});
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon?limit=807`)
+      .then(function (response) {
+        const { data } = response;
+        const { results } = data;
+        const newPokemonData = {};
+        results.forEach((pokemon, index) => {
+          newPokemonData[index + 1] = {
+            id: index + 1,
+            name: pokemon.name,
+            sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+              index + 1
+            }.png`,
+          };
+        });
+        setPokemonData(newPokemonData);
+      });
+  }, []);
+
+  const handleSearchChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const getPokemonCard = (pokemonId) => {
+    const { id, name, sprite } = pokemonData[pokemonId];
+    return (
+      <Grid item xs={4} key={pokemonId}>
+        <Card onClick={() => history.push(`/${id}`)}>
+          <CardMedia
+            className={classes.cardMedia}
+            image={sprite}
+            style={{ width: "130px", height: "130px" }}
+          />
+          <CardContent className={classes.cardContent}>
+            <Typography>{`${id}. ${toFirstCharUppercase(name)}`}</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+  };
+
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <div className={classes.searchContainer}>
+            <SearchIcon className={classes.searchIcon} />
+            <TextField
+              className={classes.searchInput}
+              onChange={handleSearchChange}
+              label="Pokemon"
+              variant="standard"
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+      {pokemonData ? (
+        <Grid container spacing={2} className={classes.pokedexContainer}>
+          {Object.keys(pokemonData).map(
+            (pokemonId) =>
+
+            // {id === 1 ? (
+            //     <button>imagem3d</button>
+    
+            //   ) : (
+            //     <button>não have</button>
+            //   )},
+
+              pokemonData[pokemonId].name.includes(filter) &&
+              getPokemonCard(pokemonId)
+          )}
+        </Grid>
+      ) : (
+        <CircularProgress />
+      )}
+    </>
+  );
+};
+
+export default Pokedex;
